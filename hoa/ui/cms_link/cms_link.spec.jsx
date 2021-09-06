2c178e074a9abab8ee9286e7176c59a76a06f827
Brian Alexander <balexand@gmail.com>
2021-09-06 14:39:55 -0600

    Update cms_link.spec.jsx

--- hoa/ui/cms_link/cms_link.spec.jsx
+++ hoa/ui/cms_link/cms_link.spec.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { BasicCmsLink } from './cms_link.composition';
+import { CMSLink } from './cms_link';
+import { RedExternalLink } from './cms_link.composition';
 
-it('should render with the correct text', () => {
-  const { getByText } = render(<BasicCmsLink />);
-  const rendered = getByText('hello from CMSLink');
+CMSLink.getInternalLink = (link) => [
+  (props) => <a {...props} />,
+  { href: link.document.path.current, children: 'Internal Link' },
+];
+
+it('should render CMSLink with internal link', () => {
+  const { getByText } = render(
+    <CMSLink
+      link={{
+        _key: 'd3d49d559627',
+        _type: 'internalLink',
+        document: {
+          _type: 'collection',
+          path: { _type: 'slug', current: '/apparel' },
+        },
+      }}
+    >
+      Internal Link
+    </CMSLink>
+  );
+  const rendered = getByText('Internal Link');
+
   expect(rendered).toBeTruthy();
+  expect(rendered.href).toEqual('http://localhost/apparel');
+  expect(rendered.rel).toEqual('');
+  expect(rendered.target).toEqual('');
+});
+
+it('should render RedExternalLink', () => {
+  const { getByText } = render(<RedExternalLink />);
+  const rendered = getByText('External Link');
+
+  expect(rendered).toBeTruthy();
+  expect(rendered.href).toEqual('http://example.com/');
+  expect(rendered.rel).toEqual('noopener noreferrer');
+  expect(rendered.target).toEqual('_blank');
 });
