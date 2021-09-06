5ae9124464fdc89ff4f2faabec3fe69f76ef1148
Brian Alexander <balexand@gmail.com>
2021-09-06 15:14:41 -0600

    config

--- eslint/.eslintrc.js
+++ eslint/.eslintrc.js
@@ -1,23 +1,9 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  extends: ['@houseofathlete/eslint-config-hoa'],
+
+  env: {
+    'jest/globals': true,
+  },
+
+  plugins: ['jest'],
 };
