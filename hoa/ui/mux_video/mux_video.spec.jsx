2f64e716aaf4cf277df69da7b8a677a335abeb20
Brian Alexander <balexand@gmail.com>
2021-10-28 10:21:47 -0600

    Delete mux_video.spec.jsx

--- hoa/ui/mux_video/mux_video.spec.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { render } from "@testing-library/react"
-import { BasicMuxVideo } from "./mux_video.composition"
-
-it("should render without error", () => {
-  render(<BasicMuxVideo />)
-})
