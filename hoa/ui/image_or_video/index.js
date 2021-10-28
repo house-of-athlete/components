2f6b080253020434cc3b7ec3e8d736d125c72c16
Brian Alexander <balexand@gmail.com>
2021-10-28 12:12:59 -0600

    projections

--- hoa/ui/image_or_video/index.js
+++ hoa/ui/image_or_video/index.js
@@ -1,1 +1,10 @@
+import { imageProjection } from "@hoa/hoa.ui.sanity_image"
+
 export { ImageOrVideo } from "./image_or_video"
+
+export const imageOrVideoProjection = `{
+  "image": image.asset -> ${imageProjection},
+  "imagePhone": imagePhone.asset -> ${imageProjection},
+  "video": video.asset -> ${muxVideoProjection},
+  "videoPhone": videoPhone.asset -> ${muxVideoProjection},
+}`
--- hoa/ui/sanity_image/index.js
+++ hoa/ui/sanity_image/index.js
@@ -5,3 +5,15 @@ export {
 } from "./sanity_image"
 
 export { sanityImageUrl } from "./sanityUtil"
+
+export const imageProjection = `{
+  _id,
+  _type,
+  metadata {
+    dimensions,
+    palette {
+      dominant
+    },
+  },
+  url,
+}`.replace(/\n/g, "")
