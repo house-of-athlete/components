2f3066855a8b3f3587d57f478b1d4d9be5f788ee
Brian Alexander <balexand@gmail.com>
2021-10-28 17:07:57 -0600

    include player icon here instead of in app (#10)
    * include player icon here instead of in app
    
    * Update .bitmap
    
    * export
    
    * Update .bitmap
    
    * image asset causing errors in IAA project; just hardcode SVG
    
    * Update .bitmap
    
    * Update play_button_overlay.jsx
    
    * Update .bitmap

---
+++ hoa/ui/mux_video/play_button_overlay.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+export const PlayButtonOverlay = () => (
+  <div className="absolute flex items-center inset-0 justify-center pointer-events-none">
+    <div
+      className="bg-opacity-25 bg-white flex h-32 items-center justify-center w-40"
+      style={{ borderRadius: "30px" }}
+    >
+      <svg
+        className="w-6"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="30.93000030517578 0 148.1400146484375 210"
+      >
+        <path d="M179.07,105L30.93,210V0L179.07,105z" fill="white" />
+      </svg>
+    </div>
+  </div>
+)
+
+PlayButtonOverlay.propTypes = {}
