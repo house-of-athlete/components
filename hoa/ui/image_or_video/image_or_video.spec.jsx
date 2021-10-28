7a4b548ca2050fb98695eaa1c25fa790525e464e
Brian Alexander <balexand@gmail.com>
2021-10-28 12:00:54 -0600

    fix composition and test

--- hoa/ui/image_or_video/image_or_video.spec.jsx
+++ hoa/ui/image_or_video/image_or_video.spec.jsx
@@ -2,8 +2,6 @@ import React from "react"
 import { render } from "@testing-library/react"
 import { BasicImageOrVideo } from "./image_or_video.composition"
 
-it("should render with the correct text", () => {
-  const { getByText } = render(<BasicImageOrVideo />)
-  const rendered = getByText("hello from ImageOrVideo")
-  expect(rendered).toBeTruthy()
+it("should render without error", () => {
+  render(<BasicImageOrVideo />)
 })
