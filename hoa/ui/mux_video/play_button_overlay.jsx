c4eb48ed6d4e985cc175bf573c7b780ae9f495c4
Brian Alexander <balexand@gmail.com>
2021-10-28 17:00:50 -0600

    image asset causing errors in IAA project; just hardcode SVG

--- hoa/ui/mux_video/play_button_overlay.jsx
+++ hoa/ui/mux_video/play_button_overlay.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import playIcon from "./play-icon.svg"
 
 export const PlayButtonOverlay = () => (
   <div className="absolute flex items-center inset-0 justify-center pointer-events-none">
@@ -7,7 +6,17 @@ export const PlayButtonOverlay = () => (
       className="bg-opacity-25 bg-white flex h-32 items-center justify-center w-40"
       style={{ borderRadius: "30px" }}
     >
-      <img className="w-6" src={playIcon} />
+      <svg
+        className="w-6"
+        version="1.1"
+        id="Capa_1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="30.93000030517578 0 148.1400146484375 210"
+        xml:space="preserve"
+      >
+        <path d="M179.07,105L30.93,210V0L179.07,105z" fill="white" />
+      </svg>
     </div>
   </div>
 )
