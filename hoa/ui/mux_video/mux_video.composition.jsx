f0ccb8d3fd0cf94bba532dd3b498dfde930c9ad3
Brian Alexander <balexand@gmail.com>
2021-10-28 10:19:59 -0600

    mux video

--- hoa/ui/mux_video/mux_video.composition.jsx
+++ hoa/ui/mux_video/mux_video.composition.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
-import { MuxVideo } from './mux_video';
+import React from "react"
+import { MuxVideo } from "./mux_video"
 
 export const BasicMuxVideo = () => (
-  <MuxVideo text="hello from MuxVideo" />
-);
+  <MuxVideo
+    style={{ height: "auto", width: "100%" }}
+    autoPlay
+    muted
+    loop
+    playsInline
+    video={{
+      _type: "mux.videoAsset",
+      data: { aspect_ratio: "4:5" },
+      playbackId: "xxpHrEQMIRfCMkKfD1xJ7yDeUjvlq00NzpUZWa02EgLIs",
+      thumbTime: null,
+    }}
+  />
+)
--- hoa/ui/mux_video/mux_video.spec.jsx
+++ hoa/ui/mux_video/mux_video.spec.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { BasicMuxVideo } from './mux_video.composition';
+import React from "react"
+import { render } from "@testing-library/react"
+import { BasicMuxVideo } from "./mux_video.composition"
 
-
-it('should render with the correct text', () => {
-  const { getByText } = render(<BasicMuxVideo />);
-  const rendered = getByText('hello from MuxVideo');
-  expect(rendered).toBeTruthy();
-});
-
+it("should render without error", () => {
+  render(<BasicMuxVideo />)
+})
