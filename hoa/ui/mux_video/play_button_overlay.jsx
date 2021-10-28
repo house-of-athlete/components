0e46973f5938e52499080da5245ae5bc17f50510
Brian Alexander <balexand@gmail.com>
2021-10-28 17:12:39 -0600

    React doesn't like `xmlns` props on `<svg>`

--- hoa/ui/mux_video/play_button_overlay.jsx
+++ hoa/ui/mux_video/play_button_overlay.jsx
@@ -6,13 +6,7 @@ export const PlayButtonOverlay = () => (
       className="bg-opacity-25 bg-white flex h-32 items-center justify-center w-40"
       style={{ borderRadius: "30px" }}
     >
-      <svg
-        className="w-6"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        viewBox="30.93000030517578 0 148.1400146484375 210"
-      >
+      <svg className="w-6" viewBox="30.93000030517578 0 148.1400146484375 210">
         <path d="M179.07,105L30.93,210V0L179.07,105z" fill="white" />
       </svg>
     </div>
