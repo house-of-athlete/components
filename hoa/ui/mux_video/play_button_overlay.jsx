182930c945a82a6f2110c3b771f162646644aaa9
Brian Alexander <balexand@gmail.com>
2021-10-28 17:04:21 -0600

    Update play_button_overlay.jsx

--- hoa/ui/mux_video/play_button_overlay.jsx
+++ hoa/ui/mux_video/play_button_overlay.jsx
@@ -9,11 +9,9 @@ export const PlayButtonOverlay = () => (
       <svg
         className="w-6"
         version="1.1"
-        id="Capa_1"
         xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink"
         viewBox="30.93000030517578 0 148.1400146484375 210"
-        xml:space="preserve"
       >
         <path d="M179.07,105L30.93,210V0L179.07,105z" fill="white" />
       </svg>
