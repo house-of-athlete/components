8a507a9f7a3f0951fb672aae5c82c6252858a555
Brian Alexander <balexand@gmail.com>
2021-11-01 13:10:48 -0600

    Update prevent_scrolling.spec.jsx

--- hoa/ui/prevent_scrolling/prevent_scrolling.spec.jsx
+++ hoa/ui/prevent_scrolling/prevent_scrolling.spec.jsx
@@ -2,8 +2,6 @@ import React from "react"
 import { render } from "@testing-library/react"
 import { BasicPreventScrolling } from "./prevent_scrolling.composition"
 
-it("should render with the correct text", () => {
-  const { getByText } = render(<BasicPreventScrolling />)
-  const rendered = getByText("hello from PreventScrolling")
-  expect(rendered).toBeTruthy()
+it("should render without error", () => {
+  render(<BasicPreventScrolling />)
 })
