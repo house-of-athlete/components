9050d34b29afa7dbadebf32a4732179e1da5347b
Brian Alexander <balexand@gmail.com>
2021-11-01 14:07:58 -0600

    Update modal.spec.jsx

--- hoa/ui/modal/modal.spec.jsx
+++ hoa/ui/modal/modal.spec.jsx
@@ -4,6 +4,6 @@ import { BasicModal } from "./modal.composition"
 
 it("should render with the correct text", () => {
   const { getByText } = render(<BasicModal />)
-  const rendered = getByText("hello from Modal")
+  const rendered = getByText("Content goes here.")
   expect(rendered).toBeTruthy()
 })
