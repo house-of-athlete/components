3a80709263bcad592fa0029faf7870aa9a09b0c7
Brian Alexander <balexand@gmail.com>
2021-11-01 13:57:10 -0600

    generate modal

---
+++ hoa/ui/modal/modal.spec.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BasicModal } from './modal.composition';
+
+
+it('should render with the correct text', () => {
+  const { getByText } = render(<BasicModal />);
+  const rendered = getByText('hello from Modal');
+  expect(rendered).toBeTruthy();
+});
+
