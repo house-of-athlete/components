6d8f10329e5f6c356ae11469999670517132d44e
Brian Alexander <balexand@gmail.com>
2021-11-01 13:38:31 -0600

    Update event_link.spec.jsx

--- hoa/ui/event_link/event_link.spec.jsx
+++ hoa/ui/event_link/event_link.spec.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { BasicEventLink } from './event_link.composition';
+import React from "react"
+import { render } from "@testing-library/react"
+import { BasicEventLink } from "./event_link.composition"
 
-
-it('should render with the correct text', () => {
-  const { getByText } = render(<BasicEventLink />);
-  const rendered = getByText('hello from EventLink');
-  expect(rendered).toBeTruthy();
-});
-
+it("should render with the correct text", () => {
+  const { getByText } = render(<BasicEventLink />)
+  const rendered = getByText("Click me")
+  expect(rendered).toBeTruthy()
+})
