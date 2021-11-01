c42009243397219d0dab610e7978bc94d6ea2947
Brian Alexander <balexand@gmail.com>
2021-11-01 13:48:17 -0600

    remove generated content

--- hoa/ui/prevent_scrolling/prevent_scrolling.composition.jsx
+++ hoa/ui/prevent_scrolling/prevent_scrolling.composition.jsx
@@ -1,6 +1,4 @@
 import React from "react"
 import { PreventScrolling } from "./prevent_scrolling"
 
-export const BasicPreventScrolling = () => (
-  <PreventScrolling text="hello from PreventScrolling" />
-)
+export const BasicPreventScrolling = () => <PreventScrolling />
