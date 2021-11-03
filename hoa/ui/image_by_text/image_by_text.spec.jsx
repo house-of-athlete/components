634aa2486a700715cd38f46060991a38f25a3a2d
Brian Alexander <balexand@gmail.com>
2021-11-03 17:17:32 -0600

    Update image_by_text.spec.jsx

--- hoa/ui/image_by_text/image_by_text.spec.jsx
+++ hoa/ui/image_by_text/image_by_text.spec.jsx
@@ -2,8 +2,6 @@ import React from "react"
 import { render } from "@testing-library/react"
 import { BasicImageByText } from "./image_by_text.composition"
 
-it("should render with the correct text", () => {
-  const { getByText } = render(<BasicImageByText />)
-  const rendered = getByText("hello from ImageByText")
-  expect(rendered).toBeTruthy()
+it("should render without error", () => {
+  render(<BasicImageByText />)
 })
