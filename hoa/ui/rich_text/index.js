72436d5c2cd5e5d3bb25d461506005169dc8fb1a
Brian Alexander <balexand@gmail.com>
2021-11-03 16:44:01 -0600

    missing export

--- hoa/ui/rich_text/index.js
+++ hoa/ui/rich_text/index.js
@@ -1,7 +1,8 @@
 import { internalLinkProjection } from "@hoa/hoa.ui.cms_link"
 import { muxVideoProjection } from "@hoa/hoa.ui.mux_video"
 import { socialItemProjection } from "@hoa/hoa.ui.social_links"
-export { RichText } from "./rich_text"
+
+export { RichText, RichTextConfigProvider } from "./rich_text"
 
 export const blockContentProjection = `{
   ...,
