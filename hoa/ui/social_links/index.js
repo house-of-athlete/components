ae5c6c9844a938e386c5be4fadf29ae8627e3ac6
Brian Alexander <balexand@gmail.com>
2021-11-03 13:10:17 -0600

    export projection

--- hoa/ui/social_links/index.js
+++ hoa/ui/social_links/index.js
@@ -1,1 +1,10 @@
 export { SocialLinks } from "./social_links"
+
+export const socialItemProjection = `{
+  ...,
+
+  socialNetwork -> {
+    "icon": icon.asset ->  ${imageProjection},
+    title,
+  },
+}`
