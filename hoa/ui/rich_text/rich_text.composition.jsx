e5f7461c2d16dc3d92cd5c0503093f71d0f83ac4
Brian Alexander <balexand@gmail.com>
2021-11-03 13:44:40 -0600

    `RichText` component

--- hoa/ui/rich_text/rich_text.composition.jsx
+++ hoa/ui/rich_text/rich_text.composition.jsx
@@ -1,6 +1,96 @@
-import React from 'react';
-import { RichText } from './rich_text';
+import React from "react"
+import { RichText, StyledRichText } from "./rich_text"
 
-export const BasicRichText = () => (
-  <RichText text="hello from RichText" />
-);
+const brandonBlocks = [
+  {
+    _key: "ee9405d44c92",
+    _type: "block",
+    children: [{ _key: "165e9865d714", _type: "span", marks: [], text: "" }],
+    markDefs: [],
+    style: "normal",
+  },
+  {
+    _key: "5bff1b5082ef",
+    _type: "block",
+    children: [
+      {
+        _key: "fffdb530467d",
+        _type: "span",
+        marks: [],
+        text: "Brandon Marshall spent 12 years in the National Football League as one of the top wide receivers in the game. He would spend his career shattering records and collecting countless awards and accolades. Now, just a few years into his retirement Brandon continues to exceed expectations and redefine what it means to be an athlete.",
+      },
+    ],
+    markDefs: [],
+    style: "normal",
+  },
+  {
+    _key: "30b35c1b3e54",
+    _type: "block",
+    children: [
+      {
+        _key: "3c12bd077f020",
+        _type: "span",
+        marks: [],
+        text: "As the founder and CEO of House of Athlete, a lifestyle wellness brand giving every athlete, of all levels, access to the top-tier resources necessary to unlock their full potential and train like a pro. Brandon is utilizing his many years of experience with mental and physical health, to build an empire of limitless potential.",
+      },
+    ],
+    markDefs: [],
+    style: "normal",
+  },
+  {
+    _key: "ae5fefb2484a",
+    _type: "blockContentSocialLinks",
+    markDefs: null,
+    socialItems: [
+      {
+        _key: "c2cb1f06a9e1",
+        _type: "socialLink",
+        socialNetwork: {
+          icon: {
+            _id: "image-e29754b96f0c743e5def6d269b55fc1771b285ec-24x24-svg",
+            metadata: {
+              dimensions: {
+                _type: "sanity.imageDimensions",
+                aspectRatio: 1,
+                height: 24,
+                width: 24,
+              },
+              palette: {
+                dominant: {
+                  _type: "sanity.imagePaletteSwatch",
+                  background: "#040404",
+                  foreground: "#fff",
+                  population: 100,
+                  title: "#fff",
+                },
+              },
+            },
+            url: "https://cdn.sanity.io/images/xriwku0s/production/e29754b96f0c743e5def6d269b55fc1771b285ec-24x24.svg",
+          },
+          title: "Instagram",
+        },
+        url: "https://www.instagram.com/bmarshall/",
+      },
+    ],
+  },
+]
+
+// export const BasicRichText = () => (
+//   <RichText
+//     blocks={brandonBlocks}
+//     sanityConfig={{
+//       dataset: "production",
+//       projectId: "s7bcqajt",
+//     }}
+//   />
+// )
+
+export const BasicStyledRichText = () => (
+  <StyledRichText
+    blocks={brandonBlocks}
+    sanityConfig={{
+      dataset: "production",
+      projectId: "s7bcqajt",
+    }}
+  />
+)
--- hoa/ui/rich_text/rich_text.jsx
+++ hoa/ui/rich_text/rich_text.jsx
@@ -1,9 +1,93 @@
-import React from 'react';
+import BlockContent from "@sanity/block-content-to-react"
+import PropTypes from "prop-types"
+import React from "react"
+import styled from "@emotion/styled"
+import BlockRenderer from "./serializers/BlockRenderer"
+import InternalLinkSerializer from "./serializers/InternalLinkSerializer"
+import LinkSerializer from "./serializers/LinkSerializer"
+import SmallSerializer from "./serializers/SmallSerializer"
+import SocialLinksSerializer from "./serializers/SocialLinksSerializer"
+import VideoModalSerializer from "./serializers/VideoModalSerializer"
 
-export function RichText({ text }) {
+const serializers = {
+  marks: {
+    internalLink: InternalLinkSerializer,
+    link: LinkSerializer,
+    videoModal: VideoModalSerializer,
+    small: SmallSerializer,
+  },
+  types: {
+    block: BlockRenderer,
+    blockContentSocialLinks: SocialLinksSerializer,
+  },
+}
+
+export const RichText = ({ blocks, className, sanityConfig }) => {
   return (
-    <div>
-      {text}
-    </div>
-  );
+    <BlockContent
+      blocks={blocks}
+      className={className}
+      ignoreUnknownTypes={false}
+      projectId={sanityConfig.projectId}
+      dataset={sanityConfig.dataset}
+      serializers={serializers}
+    />
+  )
+}
+
+RichText.propTypes = {
+  blocks: PropTypes.array.isRequired,
+  className: PropTypes.string,
+  sanityConfig: PropTypes.object.isRequired,
+}
+
+const Styled = styled.div`
+  line-height: 1.5;
+
+  a {
+    text-decoration: underline;
+  }
+
+  p,
+  ul,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  ul {
+    margin: 14px 0;
+  }
+
+  ol,
+  ul {
+    padding-left: 40px;
+  }
+
+  ol li {
+    list-style-type: decimal;
+  }
+
+  ul li {
+    list-style-type: disc;
+  }
+
+  p:first-of-type,
+  ul:first-of-type {
+    margin-top: 0;
+  }
+  p:last-of-type,
+  ul:last-of-type {
+    margin-bottom: 0;
+  }
+`
+
+export const StyledRichText = ({ className, ...props }) => (
+  <Styled className={className}>
+    <RichText {...props} />
+  </Styled>
+)
+
+StyledRichText.propTypes = {
+  className: PropTypes.string,
 }
