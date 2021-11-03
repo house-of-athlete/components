9456b8e76e90f465d3dd52cccbee18f66d47ca1a
Brian Alexander <balexand@gmail.com>
2021-11-03 13:49:33 -0600

    export projection

--- hoa/ui/rich_text/index.js
+++ hoa/ui/rich_text/index.js
@@ -1,1 +1,23 @@
+import { muxVideoProjection } from "@hoa/hoa.ui.mux_video"
+import { socialItemProjection } from "@hoa/hoa.ui.social_links"
 export { RichText } from "./rich_text"
+
+export const blockContentProjection = `{
+  ...,
+
+  markDefs[] {
+    ...,
+
+    _type == "internalLink" => {
+      doc -> ${internalLinkProjection},
+    },
+
+    _type == "videoModal" => {
+      "video": video.asset -> ${muxVideoProjection},
+    },
+  },
+
+  _type == "blockContentSocialLinks" => {
+    socialItems[] ${socialItemProjection},
+  },
+}`
