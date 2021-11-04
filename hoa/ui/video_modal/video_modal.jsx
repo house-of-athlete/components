6aebf0377b72cdca338d6d055b41b7d53157f20b
Brian Alexander <balexand@gmail.com>
2021-11-04 11:40:36 -0600

    Refactor `VideoModal` (#15)
    * refactor CSS variables
    
    * refactor `VideoModal` to not use `Modal`
    
    * remove modal component
    
    * Update .bitmap

--- hoa/ui/video_modal/video_modal.jsx
+++ hoa/ui/video_modal/video_modal.jsx
@@ -1,13 +1,38 @@
 import PropTypes from "prop-types"
 import React, { useState } from "react"
+import styled from "@emotion/styled"
 import { EventLink } from "@hoa/hoa.ui.event_link"
-import { Modal } from "@hoa/hoa.ui.modal"
+import { CloseButton } from "@hoa/hoa.ui.close_button"
 import { MuxVideo } from "@hoa/hoa.ui.mux_video"
+import { PreventScrolling } from "@hoa/hoa.ui.prevent_scrolling"
+
+const StyledCloseButton = styled(CloseButton)`
+  --hoa-close-button-color: var(--hoa-video-modal-close-button-color, #fff);
+`
 
 export const VideoModal = ({ onClose, video }) => (
-  <Modal onClose={onClose}>
-    <MuxVideo autoPlay controls className="max-h-full w-full" video={video} />
-  </Modal>
+  <>
+    <PreventScrolling />
+
+    <div className="fixed flex flex-col inset-0 justify-center p-6 z-50">
+      <div
+        className="absolute inset-0"
+        onClick={onClose}
+        style={{
+          backgroundColor: "var(--hoa-video-modal-backdrop-color, black)",
+          opacity: "var(--hoa-video-modal-backdrop-opacity, 0.9)",
+          zIndex: "-1",
+        }}
+      />
+
+      <StyledCloseButton
+        className="absolute p-2 right-4 top-4 w-4"
+        onClick={onClose}
+      />
+
+      <MuxVideo autoPlay controls className="max-h-full w-full" video={video} />
+    </div>
+  </>
 )
 
 VideoModal.propTypes = {
