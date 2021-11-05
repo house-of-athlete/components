2ee54718e3783debd0a5e45a60788dc348a35413
Brian Alexander <balexand@gmail.com>
2021-11-05 12:27:00 -0600

    Update banner.spec.jsx

--- hoa/ui/banner/banner.spec.jsx
+++ hoa/ui/banner/banner.spec.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { BasicBanner } from './banner.composition';
+import React from "react"
+import { render } from "@testing-library/react"
+import { BasicBanner } from "./banner.composition"
 
-
-it('should render with the correct text', () => {
-  const { getByText } = render(<BasicBanner />);
-  const rendered = getByText('hello from Banner');
-  expect(rendered).toBeTruthy();
-});
-
+it("should render without error", () => {
+  render(<BasicBanner />)
+})
