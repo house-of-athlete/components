3fc33fa6ef55e9dde9b965d1079d601918395e58
Brian Alexander <balexand@gmail.com>
2021-11-08 15:39:04 -0700

    Update grid.spec.jsx

--- hoa/ui/grid/grid.spec.jsx
+++ hoa/ui/grid/grid.spec.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import { BasicGrid } from "./grid.composition"
+import { CMSLink } from "@hoa/hoa.ui.cms_link"
 
-it("should render with the correct text", () => {
-  const { getByText } = render(<BasicGrid />)
-  const rendered = getByText("hello from Grid")
-  expect(rendered).toBeTruthy()
+CMSLink.getInternalLink = document => [
+  props => <a {...props} />,
+  { href: document.path.current, children: "Internal Link" },
+]
+
+it("should render without error", () => {
+  render(<BasicGrid />)
 })
