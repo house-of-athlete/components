afc57adb348a91356d514818a1446cfb897fa6df
Brian Alexander <balexand@gmail.com>
2021-11-10 11:43:16 -0700

    fix banner test

--- hoa/ui/banner/banner.spec.jsx
+++ hoa/ui/banner/banner.spec.jsx
@@ -1,6 +1,12 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import { BasicBanner } from "./banner.composition"
+import { CMSLink } from "@hoa/hoa.ui.cms_link"
+
+CMSLink.getInternalLink = document => [
+  props => <a {...props} />,
+  { href: document.path.current, children: "Internal Link" },
+]
 
 it("should render without error", () => {
   render(<BasicBanner />)
