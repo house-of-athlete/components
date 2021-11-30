5b2a17913f11e0de13c46bd949a027dbd1ad993b
Brian Alexander <balexand@gmail.com>
2021-11-30 12:58:05 -0700

    delete image_by_text

--- hoa/ui/image_by_text/image_by_text.spec.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { render } from "@testing-library/react"
-import { BasicImageByText } from "./image_by_text.composition"
-
-it("should render without error", () => {
-  render(<BasicImageByText />)
-})
