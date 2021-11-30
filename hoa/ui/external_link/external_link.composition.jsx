6bf9d2309ca10b976a07584f80bac300e1e382c4
Brian Alexander <balexand@gmail.com>
2021-11-30 13:15:29 -0700

    delete external_link

--- hoa/ui/external_link/external_link.composition.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { ExternalLink } from "./external_link"
-
-export const BasicExternalLink = () => (
-  <ExternalLink href="http://example.com/">Hello</ExternalLink>
-)
