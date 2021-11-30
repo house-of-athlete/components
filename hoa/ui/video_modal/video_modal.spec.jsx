17425f153f2badfc2f0dd721148c3f596fcc0b91
Brian Alexander <balexand@gmail.com>
2021-11-30 13:19:56 -0700

    delete video_modal

--- hoa/ui/video_modal/video_modal.spec.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { render } from "@testing-library/react"
-import { BasicVideoModalLink } from "./video_modal.composition"
-
-it("should render link", () => {
-  const { getByText } = render(<BasicVideoModalLink />)
-  const rendered = getByText("Click here to open video in modal")
-  expect(rendered).toBeTruthy()
-})
